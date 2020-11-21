4120d39d69d35cc8c2b59b26849a60358ec80175
Ardovigus <aureliusmarcelmc@gmail.com>
2020-11-21 14:46:09 +0700

    revisi map

--- map_navigasi_sementara.pl
+++ map_navigasi_sementara.pl
@@ -1,191 +1,191 @@
-:- dynamic(koord_P_X/1).
-:- dynamic(koord_P_Y/1).
+:- dynamic(positionX/1).
+:- dynamic(positionY/1).
 :- dynamic(shop/2).
 :- dynamic(boss_dungeon/2).
 
 initial :- 
-	asserta(koord_P_X(1)),
-	asserta(koord_P_Y(1)),
-	asserta(shop(11,11)),           /* --> Bisa diganti* */
-	asserta(boss_dungeon(15,15)).   /* --> Bisa diganti* */
+	asserta(positionX(1)),
+	asserta(positionY(1)),
+	asserta(shop(11,11)),               % --> Bisa diganti*
+	asserta(boss_dungeon(15,15)).       % --> Bisa diganti*
 
-/* * : Tidak menggunakan random, karena hasil random selalu sama untuk langkah ke-N pada console */
-
-/* Perintah Navigasi: */
-/* Gerak ke atas */
+% * : Tidak menggunakan random, karena hasil random selalu sama untuk langkah ke-N pada console
+% Perintah Navigasi:
+% Gerak ke atas
 w :-
-    koord_P_X(TempX),
-    koord_P_Y(Temp),
+    positionX(TempX),
+    positionY(Temp),
     Next is (Temp - 1),
 	(
-	Next =:= 0 ->
-        write('Kena Batas')
+	Next =:= 0 -> write('Kena Batas')
 	;
-    shop(X1,Y1), TempX =:= X1, Next =:= Y1 ->
-        write('Shop boi'),
-        retract(koord_P_Y(_)),
-        asserta(koord_P_Y(Next)),!,
-        write('['),write(Next),write(','),write(TempX),write(']')
-    ;
-    boss_dungeon(X2,Y2), TempX =:= X2, Next =:= Y2 ->
-        write('Boss boi'),
-        retract(koord_P_Y(_)),
-        asserta(koord_P_Y(Next)),!,
-        write('['),write(Next),write(','),write(TempX),write(']')
-    ;
-	retract(koord_P_Y(_)),
-	asserta(koord_P_Y(Next)),!,
+	shop(X1,Y1), TempX =:= X1, Next =:= Y1 ->
+		write('Shop boi'),
+		retract(positionY(_)),
+		asserta(positionY(Next)),!,
+		write('['),write(Next),write(','),write(TempX),write(']')
+	;
+	boss_dungeon(X2,Y2), TempX =:= X2, Next =:= Y2 ->
+		write('Boss boi'),
+		retract(positionY(_)),
+		asserta(positionY(Next)),!,
+		write('['),write(Next),write(','),write(TempX),write(']')
+	;
+	retract(positionY(_)),
+	asserta(positionY(Next)),!,
 	write('['),write(Next),write(','),write(TempX),write(']')
 	).
 
-/* Gerak ke kiri */
+% Gerak ke kiri
 a :-
-    koord_P_X(TempX),
-    koord_P_Y(Temp),
+    positionX(TempX),
+    positionY(Temp),
     Next is (TempX - 1),
 	(
 	Next =:= 0 -> write('Kena Batas')
 	;
-    shop(X1,Y1), Temp =:= Y1, Next =:= X1 ->
-        write('Shop boi'),
-        retract(koord_P_X(_)),
-        asserta(koord_P_X(Next)),!,
-        write('['),write(Temp),write(','),write(Next),write(']')
-    ;
-    boss_dungeon(X2,Y2), Temp =:= Y2, Next =:= X2 ->
-        write('Boss boi'),
-        retract(koord_P_X(_)),
-        asserta(koord_P_X(Next)),!,
-        write('['),write(Temp),write(','),write(Next),write(']')
-    ;
-	retract(koord_P_X(_)),
-	asserta(koord_P_X(Next)),!,
+	shop(X1,Y1), Temp =:= Y1, Next =:= X1 ->
+		write('Shop boi'),
+		retract(positionX(_)),
+		asserta(positionX(Next)),!,
+		write('['),write(Temp),write(','),write(Next),write(']')
+	;
+	boss_dungeon(X2,Y2), Temp =:= Y2, Next =:= X2 ->
+		write('Boss boi'),
+		retract(positionX(_)),
+		asserta(positionX(Next)),!,
+		write('['),write(Temp),write(','),write(Next),write(']')
+	;
+	retract(positionX(_)),
+	asserta(positionX(Next)),!,
 	write('['),write(Temp),write(','),write(Next),write(']')
 	).
 
-/* Gerak ke bawah */
+% Gerak ke bawah
 s :-
-    koord_P_X(TempX),
-    koord_P_Y(Temp),
+    positionX(TempX),
+    positionY(Temp),
     Next is (Temp + 1),
 	(
-	Next =:= 19 ->
-        write('Kena Batas')
+	Next =:= 19 -> write('Kena Batas')
 	;
-    shop(X1,Y1), TempX =:= X1, Next =:= Y1 ->
-        write('Shop boi'),
-        retract(koord_P_Y(_)),
-        asserta(koord_P_Y(Next)),!,
-        write('['),write(Next),write(','),write(TempX),write(']')        
-    ;
-    boss_dungeon(X2,Y2), TempX =:= X2, Next =:= Y2 ->
-        write('Boss boi'),
-        	retract(koord_P_Y(_)),
-            asserta(koord_P_Y(Next)),!,
-            write('['),write(Next),write(','),write(TempX),write(']')
-    ;
- 	retract(koord_P_Y(_)),
-	asserta(koord_P_Y(Next)),!,
+	shop(X1,Y1), TempX =:= X1, Next =:= Y1 ->
+		write('Shop boi'),
+		retract(positionY(_)),
+		asserta(positionY(Next)),!,
+		write('['),write(Next),write(','),write(TempX),write(']')
+	;
+	boss_dungeon(X2,Y2), TempX =:= X2, Next =:= Y2 ->
+		write('Boss boi'),
+		retract(positionY(_)),
+		asserta(positionY(Next)),!,
+		write('['),write(Next),write(','),write(TempX),write(']')
+	;
+	retract(positionY(_)),
+	asserta(positionY(Next)),!,
 	write('['),write(Next),write(','),write(TempX),write(']')
 	).
 
-/* Gerak ke kanan */
+% Gerak ke kanan
 d :-
-    koord_P_X(TempX),
-    koord_P_Y(Temp),
+    positionX(TempX),
+    positionY(Temp),
     Next is (TempX + 1),
 	(
-	Next =:= 19 ->
-        write('Kena Batas')
+	Next =:= 19 -> write('Kena Batas')
 	;
-    shop(X1,Y1), Temp =:= Y1, Next =:= X1 ->
-        write('Shop boi'),
-        retract(koord_P_X(_)),
-        asserta(koord_P_X(Next)),!,
-        write('['),write(Temp),write(','),write(Next),write(']')
-    ;
-    boss_dungeon(X2,Y2), Temp =:= Y2, Next =:= X2 ->
-        write('Boss boi'),
-    	retract(koord_P_X(_)),
-        asserta(koord_P_X(Next)),!,
-        write('['),write(Temp),write(','),write(Next),write(']')
-    ;
-	retract(koord_P_X(_)),
-	asserta(koord_P_X(Next)),!,
+	shop(X1,Y1), Temp =:= Y1, Next =:= X1 ->
+		write('Shop boi'),
+		retract(positionX(_)),
+		asserta(positionX(Next)),!,
+		write('['),write(Temp),write(','),write(Next),write(']')
+	;
+	boss_dungeon(X2,Y2), Temp =:= Y2, Next =:= X2 ->
+		write('Boss boi'),
+		retract(positionX(_)),
+		asserta(positionX(Next)),!,
+		write('['),write(Temp),write(','),write(Next),write(']')
+	;
+	retract(positionX(_)),
+	asserta(positionX(Next)),!,
 	write('['),write(Temp),write(','),write(Next),write(']')
 	).
 
-/* Print Map */
-printX(X,Y) :-
-    (
-	Y =:= 0 ->          /* Print batas atas */
-        write('X')
-	),
-    NextX is (X + 1),
-    printX(NextX,Y).
-
-printX(X,Y) :-
-    (
-	X =:= 0 ->          /* Print batas kiri */
-        write('X')
-	),
-    NextX is (X + 1),
-    printX(NextX,Y).
-
+% Print map
+% Mencapai kanan bawah
 printX(X,Y) :-
 	(
-	Y =:= 19 ->         /* Print batas bawah */
-        write('X')
-	),
-    NextX is (X + 1),
-    printX(NextX,Y).
-
-printX(X,Y) :-
-    (
-	X =:= 19 ->         /* Print batas kanan */
-        write('X'),
-        nl
-	),
-    NextX is 0,         /* Next: Turun 1, kembali ke ujung kiri */
-    NextY is (Y + 1),   
-    printX(NextX,NextY).
-
-printX(X,Y) :-          /* Print petak isi */
-	\+ X =:= 0,
-	\+ X =:= 19,
-	\+ Y =:= 0,
-	\+ Y =:= 19,
-    write('-'),
-    NextX is (X + 1),
-    printX(NextX,Y).
-
-printX(X,Y) :-
-    (
-	X =:= 19, Y =:= 19 ->           /* --> Sudah mencapai kanan bawah */
-        write('X')
+	X =:= 19, Y =:= 19 ->
+		write('X')
 	).
 
+% Print simbol pemain
 printX(X,Y) :-
 	(
-	koord_P_X(A), X =:= A, koord_P_Y(B), Y =:= B ->     /* Print simbol pemain */
-        write('P')
-    ),
+	positionX(A), X =:= A, positionY(B), Y =:= B ->
+		write('P')),
     NextX is (X + 1),
     printX(NextX,Y).
 
+% Print simbol shop
 printX(X,Y) :-
 	(
-	shop(X1,Y1), X =:= X1, Y =:= Y1 ->                  /* Print simbol shop */
-        write('S')
-    ),
+	shop(X1,Y1), X =:= X1, Y =:= Y1 ->
+		write('S')),
     NextX is (X + 1),
     printX(NextX,Y).
 
+% Print simbol boss_dungeon
 printX(X,Y) :-
 	(
-	boss_dungeon(X1,Y1), X =:= X1, Y =:= Y1 ->          /* Print simbol boss_dungeon */
-        write('D')
-    ),
+	boss_dungeon(X1,Y1), X =:= X1, Y =:= Y1 ->
+		write('D')),
+    NextX is (X + 1),
+    printX(NextX,Y).
+
+% Print batas bawah
+printX(X,Y) :-
+	(
+	Y =:= 19 ->
+		write('X')
+	),
+    NextX is (X + 1),
+    printX(NextX,Y).
+
+% Print batas kiri
+printX(X,Y) :-
+	(
+	X =:= 0 ->
+		write('X')
+	),
+    NextX is (X + 1),
+    printX(NextX,Y).
+
+% Print batas kanan
+printX(X,Y) :-
+	(
+	X =:= 19 ->
+		 write('X'),nl
+	),
+    NextX is 0,
+    NextY is (Y + 1),
+    printX(NextX,NextY).
+
+% Print batas atas
+printX(X,Y) :-
+	(
+	Y =:= 0 ->
+		write('X')
+	),
+    NextX is (X + 1),
+    printX(NextX,Y).
+
+% Print petak isi
+printX(X,Y) :-
+	(
+	\+ X =:= 0, \+ X =:= 19, \+ Y =:= 0, \+ Y =:= 19 ->
+    	write('-')
+	),
     NextX is (X + 1),
     printX(NextX,Y).
 
