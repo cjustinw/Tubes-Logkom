14ab40e526499254b1ff6724677cb5059f4c56e2
leonardusluwianto <leonardusluwianto@gmail.com>
2020-11-22 10:37:36 +0700

    jenis-jenis items

---
+++ items.pl
@@ -0,0 +1,30 @@
+/* Items */
+items(equipment).
+items(potion).
+
+/* Equipment */
+equipment(weapon).
+equipment(armor).
+equipment(accessory).
+
+/* Senjata */
+weapon(sword).
+weapon(arrow).
+weapon(wand).
+
+/* Damage */
+damage(sword,0.1*NewATT).
+damage(arrow,0.1*NewATT).
+damage(wand,0.1*NewATT).
+
+/* Armor */
+armor(breastplate).
+armor(shield).
+armor(incantation).
+
+/* Accessory */
+accessory(amethyst).
+accessory(emerald).
+accessory(topaz).
+
+
